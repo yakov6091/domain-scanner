eb12bec021f906ec41e630bb8c187e699aaedf81
Yakov <yakov6091@gmail.com>
2025-12-31 13:18:22 +0200

    Create demo data

---
+++ src/app/DATA/domains.ts
@@ -0,0 +1,16 @@
+export const domains = [
+    {
+        "id": 1,
+        "name": "http://facebook.com",
+        "isActive": true,
+        "lastChecked": "2025-12-27",
+        "createdAt": "2025-12-27"
+    },
+    {
+        "id": 2,
+        "name": "http://youtube.com",
+        "isActive": true,
+        "lastChecked": "2025-12-27",
+        "createdAt": "2025-12-27"
+    }
+]
